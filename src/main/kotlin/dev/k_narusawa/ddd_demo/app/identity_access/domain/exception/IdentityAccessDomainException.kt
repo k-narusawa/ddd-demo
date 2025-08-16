3d45f0a47bbb6f4611e9eaf1a4fbef69ef91ae80
k-narusawa <narusawakohei@team-lab.com>
2025-08-16 16:17:37 +0900

    refactor: 💡 sealedクラスが使えるところは修正

--- src/main/kotlin/dev/k_narusawa/ddd_demo/app/identity_access/domain/exception/IdentityAccessDomainException.kt
+++ src/main/kotlin/dev/k_narusawa/ddd_demo/app/identity_access/domain/exception/IdentityAccessDomainException.kt
@@ -1,6 +1,6 @@
 package dev.k_narusawa.ddd_demo.app.identity_access.domain.exception
 
-open class IdentityAccessDomainException(
+sealed class IdentityAccessDomainException(
   message: String? = "ドメイン例外",
   cause: Throwable? = null,
 ) : RuntimeException(message, cause)
