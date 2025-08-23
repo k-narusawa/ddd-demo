646104953248287ab88b3c737a581cfdf390e8d0
k-narusawa <narusawakohei@team-lab.com>
2025-08-23 17:33:17 +0900

    refactor: 💡 イベント処理見直し

--- src/main/kotlin/dev/k_narusawa/ddd_demo/app/identity_access/domain/activityLog/RecordLoginFailedLogWhenLoginFailedHandler.kt
+++ src/main/kotlin/dev/k_narusawa/ddd_demo/app/identity_access/domain/activityLog/RecordLoginFailedLogWhenLoginFailedHandler.kt
@@ -1,44 +1,29 @@
 package dev.k_narusawa.ddd_demo.app.identity_access.domain.activityLog
 
+import dev.k_narusawa.ddd_demo.app.identity_access.domain.exception.IdentityAccessDomainException
 import dev.k_narusawa.ddd_demo.app.identity_access.domain.user.event.LoginFailedDomainEvent
-import dev.k_narusawa.ddd_demo.app.identity_access.domain.user.event.LoginSucceededDomainEvent
-import dev.k_narusawa.ddd_demo.util.logger
+import jakarta.transaction.Transactional
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
-class ActivityLogEventListener(
+@Transactional(
+  value = Transactional.TxType.REQUIRES_NEW,
+  dontRollbackOn = [IdentityAccessDomainException::class]
+)
+class RecordLoginFailedLogWhenLoginFailedHandler(
   private val activityLogRepository: ActivityLogRepository
 ) {
-  companion object {
-    private val log = logger()
-  }
-
   @EventListener
   @Async
   fun listen(event: LoginFailedDomainEvent) {
-    log.info("${event}を受信")
     val log = ActivityLog.new(
       userId = event.user.userId,
       actionType = ActionType.LOGIN_FAILED,
       ipAddress = event.ipAddress,
       userAgent = event.userAgent,
-      occurredOn = event.occurredOn
-    )
-    activityLogRepository.save(log = log)
-  }
-
-  @EventListener
-  @Async
-  fun listen(event: LoginSucceededDomainEvent) {
-    log.info("${event}を受信")
-    val log = ActivityLog.new(
-      userId = event.user.userId,
-      actionType = ActionType.LOGIN_SUCCESS,
-      ipAddress = event.ipAddress,
-      userAgent = event.userAgent,
-      occurredOn = event.occurredOn
+      occurredOn = event.occurredAt
     )
     activityLogRepository.save(log = log)
   }
---
+++ src/main/kotlin/dev/k_narusawa/ddd_demo/app/identity_access/domain/activityLog/RecordLoginSucceededLogWhenLoginSucceededHandler.kt
@@ -0,0 +1,31 @@
+package dev.k_narusawa.ddd_demo.app.identity_access.domain.activityLog
+
+import dev.k_narusawa.ddd_demo.app.identity_access.domain.exception.IdentityAccessDomainException
+import dev.k_narusawa.ddd_demo.app.identity_access.domain.user.event.LoginSucceededDomainEvent
+import jakarta.transaction.Transactional
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+
+
+@Component
+@Transactional(
+  value = Transactional.TxType.REQUIRES_NEW,
+  dontRollbackOn = [IdentityAccessDomainException::class]
+)
+class RecordLoginSucceededLogWhenLoginSucceededHandler(
+  private val activityLogRepository: ActivityLogRepository
+) {
+  @EventListener
+  @Async
+  fun handle(event: LoginSucceededDomainEvent) {
+    val log = ActivityLog.new(
+      userId = event.user.userId,
+      actionType = ActionType.LOGIN_SUCCESS,
+      ipAddress = event.ipAddress,
+      userAgent = event.userAgent,
+      occurredOn = event.occurredAt
+    )
+    activityLogRepository.save(log = log)
+  }
+}
