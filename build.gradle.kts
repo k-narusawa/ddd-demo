89572386a5891c7f21c88146155a68ebca9eb6da
k-narusawa <narusawakohei@team-lab.com>
2025-08-24 17:50:52 +0900

    wrench: 🔧 ktlint導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,91 @@
 plugins {
-  alias(libs.plugins.kotlin.jvm)
-  alias(libs.plugins.kotlin.spring)
-  alias(libs.plugins.spring.boot)
-  alias(libs.plugins.spring.dependency.management)
-  alias(libs.plugins.kotlin.jpa)
-  alias(libs.plugins.flyway)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.flyway)
+    alias(libs.plugins.ktlint)
 }
 
 group = "dev.k-narusawa"
 version = "0.0.1-SNAPSHOT"
 
 java {
-  toolchain {
-    languageVersion = JavaLanguageVersion.of(21)
-  }
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-  mavenCentral()
-  maven { url = uri("https://repo.spring.io/milestone") }
-  maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-  implementation(libs.flyway.core)
-  implementation(libs.flyway.database.postgresql)
-  implementation(libs.spring.boot.starter.kafka)
-  testImplementation("org.springframework.kafka:spring-kafka-test")
-  runtimeOnly(libs.postgresql)
-  implementation(libs.spring.boot.starter.data.jdbc)
-  implementation(libs.spring.boot.starter.data.jpa)
-  implementation(libs.spring.boot.starter.web)
-  implementation(libs.spring.boot.starter.webflux)
-  implementation(libs.spring.boot.starter.aop)
-  implementation(libs.spring.security.crypto)
-  implementation(libs.bouncycastle.prov)
-  implementation(libs.java.jwt)
-  implementation(libs.jakarta.mail.api)
-  runtimeOnly(libs.angus.mail)
-  implementation(libs.kotlin.result)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.database.postgresql)
+    implementation(libs.spring.boot.starter.kafka)
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    runtimeOnly(libs.postgresql)
+    implementation(libs.spring.boot.starter.data.jdbc)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.webflux)
+    implementation(libs.spring.boot.starter.aop)
+    implementation(libs.spring.security.crypto)
+    implementation(libs.bouncycastle.prov)
+    implementation(libs.java.jwt)
+    implementation(libs.jakarta.mail.api)
+    runtimeOnly(libs.angus.mail)
+    implementation(libs.kotlin.result)
 
-  implementation(libs.jackson.module.kotlin)
-  implementation(libs.kotlin.reflect)
-  implementation(libs.kotlin.stdlib)
-  implementation(libs.kotlinx.coroutines.core)
-  implementation(libs.kotlinx.coroutines.reactive)
-  implementation(libs.kotlinx.coroutines.reactor)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.reactive)
+    implementation(libs.kotlinx.coroutines.reactor)
 
-  runtimeOnly(libs.flyway.core)
-  runtimeOnly(libs.flyway.database.postgresql)
+    runtimeOnly(libs.flyway.core)
+    runtimeOnly(libs.flyway.database.postgresql)
 
-  testImplementation(libs.spring.boot.starter.test)
-  testImplementation(libs.spring.boot.starter.kafka)
-  testImplementation(libs.kotlin.test.junit5)
-  testImplementation(libs.spring.security.test)
-  testRuntimeOnly(libs.junit.platform.launcher)
-  testImplementation(libs.mockk)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.starter.kafka)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.spring.security.test)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.mockk)
 }
 
 buildscript {
-  dependencies {
-    classpath(libs.flyway.database.postgresql)
-  }
+    dependencies {
+        classpath(libs.flyway.database.postgresql)
+    }
 }
 
 kotlin {
-  compilerOptions {
-    freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-  }
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
 }
 
 allOpen {
-  annotation("jakarta.persistence.Entity")
-  annotation("jakarta.persistence.MappedSuperclass")
-  annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 flyway {
-  url = "jdbc:postgresql://localhost:5432/ddd"
-  driver = "org.postgresql.Driver"
-  user = "user"
-  password = "password"
-  locations = arrayOf("filesystem:src/main/resources/db/migration")
-  cleanDisabled = false
+    url = "jdbc:postgresql://localhost:5432/ddd"
+    driver = "org.postgresql.Driver"
+    user = "user"
+    password = "password"
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
+    cleanDisabled = false
 }
