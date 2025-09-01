cd849bc0b8ddb5a49d079480d01315c6782c01fc
k-narusawa <narusawakohei@team-lab.com>
2025-09-01 14:54:27 +0900

    wrench: 🔧 datasourceの設定値を整理

--- src/main/kotlin/dev/knarusawa/dddDemo/config/DataSourceConfig.kt
+++ src/main/kotlin/dev/knarusawa/dddDemo/config/DataSourceConfig.kt
@@ -1,18 +1,39 @@
 package dev.knarusawa.dddDemo.config
 
+import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import javax.sql.DataSource
 
+@Configuration
+@EnableConfigurationProperties(TaskDatasourceConfig::class)
 class DataSourceConfig {
   @Bean
-  fun dataSource(properties: DataSourceProperties): DataSource {
-    val builder =
-      properties
-        .initializeDataSourceBuilder()
-        .type(HikariDataSource::class.java)
+  fun dataSource(taskDatasourceConfig: TaskDatasourceConfig): DataSource {
+    val hikariConfig = HikariConfig()
+    hikariConfig.apply {
+      driverClassName = "org.postgresql.Driver"
+      jdbcUrl = taskDatasourceConfig.url
+      username = taskDatasourceConfig.username
+      password = taskDatasourceConfig.password
+      connectionTestQuery = "SELECT 1"
+      minimumIdle = 5
+      maximumPoolSize = 10
+    }
 
-    return builder.build()
+    return HikariDataSource(hikariConfig)
   }
 }
+
+@ConfigurationProperties(prefix = "spring.datasource.task")
+data class TaskDatasourceConfig
+  @ConstructorBinding
+  constructor(
+    val url: String,
+    val username: String,
+    val password: String,
+  )
